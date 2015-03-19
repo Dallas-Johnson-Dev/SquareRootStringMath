0c5cf8fbfee85969c7a298c9cd3eedebf2657f83
Dallas Johnson <daljohns@nmu.edu>
2015-03-19 01:04:25 -0400

    Update Main.h
    Add comments where there needed to be.

--- SquareRootStringMath/Main.h
+++ SquareRootStringMath/Main.h
@@ -11,11 +11,11 @@
 
 using namespace std;
 
-int main(int argc, char* argv);
-string subtract(string a, string b);
-string remlzero(string s);
-bool stringEquality(string A, string B);
-void squareRoot(string value);
-string mult(string a, string b);
-string sum(string a, string b);
+int main(int argc, char* argv); //Main method, what makes everything happen
+string subtract(string a, string b); //This method is powered by 9's compliment, which was easy to implement.
+string remlzero(string s); //Removes leading zeroes, not really much of an issue.
+bool stringEquality(string A, string B); //Tests is string A is larger than string B.
+void squareRoot(string value); //does the square root of the number, and it's super complex how it happens using the second method showed to us in class.
+string mult(string a, string b); //Pre-written multiplication method using additon
+string sum(string a, string b); //Pre-written method for addition of string numbers.
 #endif __SROOT__
